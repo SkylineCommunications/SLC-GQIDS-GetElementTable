b2895f738c62b113c45bf1567a77785935485dc9
SKYLINE2\RodneyGR <rodney.graham@skyline.be>
2024-04-01 12:53:31 -0400

    Changed Sonar Cloud Project Name on .yml file.

--- GetElementTable_1/GetElementTable_1.cs
+++ GetElementTable_1/GetElementTable_1.cs
@@ -4,53 +4,60 @@ namespace GetElementTable_1
 	using System.Collections.Generic;
 	using System.Globalization;
 	using System.Text;
-	using Skyline.DataMiner.Automation;
-	
-	/// <summary>
-	/// Represents a DataMiner Automation script.
-	/// </summary>
-	public class Script
-	{
-		/// <summary>
-		/// The script entry point.
-		/// </summary>
-		/// <param name="engine">Link with SLAutomation process.</param>
-		public void Run(IEngine engine)
-		{
-			try
-			{
-				RunSafe(engine);
-			}
-			catch (ScriptAbortException)
-			{
-				// Catch normal abort exceptions (engine.ExitFail or engine.ExitSuccess)
-				throw; // Comment if it should be treated as a normal exit of the script.
-			}
-			catch (ScriptForceAbortException)
-			{
-				// Catch forced abort exceptions, caused via external maintenance messages.
-				throw;
-			}
-			catch (ScriptTimeoutException)
-			{
-				// Catch timeout exceptions for when a script has been running for too long.
-				throw;
-			}
-			catch (InteractiveUserDetachedException)
-			{
-				// Catch a user detaching from the interactive script by closing the window.
-				// Only applicable for interactive scripts, can be removed for non-interactive scripts.
-				throw;
-			}
-			catch (Exception e)
-			{
-				engine.ExitFail("Run|Something went wrong: " + e);
-			}
-		}
+    using Skyline.DataMiner.Analytics.GenericInterface;
+    using Skyline.DataMiner.Automation;
 
-		private void RunSafe(IEngine engine)
-		{
-			// TODO: Define code here
-		}
-	}
+    [GQIMetaData(Name = "Get Element Table")]
+    public class MyDataSource : IGQIDataSource, IGQIInputArguments, IGQIOnInit
+    {
+        private GQIDMS _dms;
+
+        private GQIStringArgument dmaIdArgument = new GQIStringArgument("DMA ID") { IsRequired = true };
+        private GQIStringArgument elementIdArgument = new GQIStringArgument("Element ID") { IsRequired= true };
+        private GQIStringArgument tableIdArgument = new GQIStringArgument("Table ID") { IsRequired = true };
+        private GQIStringArgument columnsIdsArguments = new GQIStringArgument("Columns IDs") { IsRequired = true };
+
+        private string dmaId;
+        private string elementId;
+        private string tableId;
+        private List<string> columnIds;
+
+
+        private List<GQIColumn> _columns;
+
+        public GQIColumn[] GetColumns()
+        {
+            return _columns.ToArray();
+        }
+
+        public GQIArgument[] GetInputArguments()
+        {
+            return new GQIArgument[] { dmaIdArgument, elementIdArgument, tableIdArgument, columnsIdsArguments };
+        }
+
+        public GQIPage GetNextPage(GetNextPageInputArgs args)
+        {
+            var rows = new List<GQIRow>();
+
+
+
+
+
+            return new GQIPage(rows.ToArray())
+            {
+                HasNextPage = false,
+            };
+        }
+
+        public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OnInitOutputArgs OnInit(OnInitInputArgs args)
+        {
+            _dms = args.DMS;
+            return new OnInitOutputArgs();
+        }
+    }
 }
